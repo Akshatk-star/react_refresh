a54ce8fcf772dc509bf325fe6cb3cdf3a22f7d2c
Ak <akshatkandpal840@gmail.com>
2026-06-15 22:53:18 +0530

    done

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import axios from 'axios'
 
 const App = () => {
+    const getdata= async ()  => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    console.log(response.data)
+    }
   return (
     <div>
-      <h1>Hello</h1>
-      <h3>Desi rap ka tora kaam chale top</h3>
+      <button onClick={getdata} className='w-30 h-30 bg-red-300 font-bold text-white'>Click Me </button>
     </div>
   )
 }
